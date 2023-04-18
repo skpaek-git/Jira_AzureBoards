d1d4a0bb96deae5e99063734284278c23c5212b2
skpaek-git <79180858+skpaek-git@users.noreply.github.com>
2023-04-18 14:37:21 +0900

    Update CreateRG.ps1

--- CreateRG.ps1
+++ CreateRG.ps1
@@ -1,6 +1,7 @@
 # $Packer 이미지를 저장하기 위한 리소스그룹 생성
 $RG = "TEST-01"
-$location = "koreacentral"
+# 실패하게끔 아래 파라미터를 주석처리함
+# $location = "koreacentral"
 
 # Create Azure ResourceGroup
 New-AzResourceGroup -Name $RG -Location $location -Force -ErrorAction:SilentlyContinue
