4713d9fae494713f7753fc1a689388466268724d
skpaek-git <79180858+skpaek-git@users.noreply.github.com>
2023-04-18 15:58:33 +0900

    TJ-1 TEST AzureDevOps_Jira

--- CreateRG.ps1
+++ CreateRG.ps1
@@ -1,6 +1,6 @@
-### $Packer 이미지를 저장하기 위한 리소스그룹 생성
+#### $Packer 이미지를 저장하기 위한 리소스그룹 생성
 $RG = "TEST-01"
 $location = "koreacentral"
 
-### Create Azure ResourceGroup
+#### Create Azure ResourceGroup
 New-AzResourceGroup -Name $RG -Location $location -Force -ErrorAction:SilentlyContinue
