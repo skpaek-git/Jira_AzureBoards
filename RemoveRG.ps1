f02deb7045559c0d95c8c4ddae28d0a081180afe
skpaek-git <79180858+skpaek-git@users.noreply.github.com>
2023-04-19 09:18:22 +0900

    Update RemoveRG.ps1

--- RemoveRG.ps1
+++ RemoveRG.ps1
@@ -1,6 +1,5 @@
-# Packer build 과정을 마치고 PackerImage Resourcegroup 을 제거
-# packer에 의해 Shared Image Gallery에 Image Version으로 업로드되기 때문에 Image를 따로 보관할 필요 없음
+# 실패! 파라미터 주석 처리로 테스트
 
-$RG = "TEST-01"
+# $RG = "TEST-01"
 
 Get-AzResourceGroup -Name $RG | Remove-AzResourceGroup -Force
