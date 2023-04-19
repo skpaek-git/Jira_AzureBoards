3b7c5da8645baed311e87683e0135aaf60df401d
skpaek-git <79180858+skpaek-git@users.noreply.github.com>
2023-04-19 10:00:25 +0900

    TJ-1 Error Commit

--- CreateRG.ps1
+++ CreateRG.ps1
@@ -1,6 +1,6 @@
 # $Packer 이미지를 저장하기 위한 리소스그룹 생성
 $RG = "TEST-01"
-$location = "koreacentral"
+# $location = "koreacentral"
 
 # Create Azure ResourceGroup
 New-AzResourceGroup -Name $RG -Location $location -Force -ErrorAction:SilentlyContinue
